f1a99158599823ec57576909fc63d73f66f4dacb
Jaitl <jaitl@outlook.com>
2021-09-13 23:00:05 +0300

    internal

--- lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Attributes.kt
+++ lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Attributes.kt
@@ -2,6 +2,6 @@ package com.github.jaitl.dynamodb.mapper
 
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 
-fun stringAttribute(str: String): AttributeValue = AttributeValue.builder().s(str).build()
-fun numberAttribute(numb: Number): AttributeValue = AttributeValue.builder().n(numb.toString()).build()
-fun mapAttribute(map: Map<String, AttributeValue>): AttributeValue = AttributeValue.builder().m(map).build()
+internal fun stringAttribute(str: String): AttributeValue = AttributeValue.builder().s(str).build()
+internal fun numberAttribute(numb: Number): AttributeValue = AttributeValue.builder().n(numb.toString()).build()
+internal fun mapAttribute(map: Map<String, AttributeValue>): AttributeValue = AttributeValue.builder().m(map).build()
