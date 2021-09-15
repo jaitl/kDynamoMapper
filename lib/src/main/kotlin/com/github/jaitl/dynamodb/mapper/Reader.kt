2c2dddd37736537d92d98b474b2fec4ce73ffaf4
Jaitl <jaitl@outlook.com>
2021-09-15 15:53:30 +0300

    read number

--- lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Reader.kt
+++ lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Reader.kt
@@ -3,6 +3,7 @@ package com.github.jaitl.dynamodb.mapper
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.full.isSubclassOf
 import kotlin.reflect.full.primaryConstructor
 
 fun <T: Any>dRead(obj: Map<String, AttributeValue>, clazz: KClass<T>): T {
@@ -25,9 +26,24 @@ internal fun matchType(param: KParameter, obj: Map<String, AttributeValue>): Any
     if (clazz.isData) {
         return dRead(attr.m(), clazz)
     }
+    if (clazz.isSubclassOf(Number::class)) {
+        return parseNumber(attr.n(), clazz)
+    }
     return when(clazz) {
-        Int::class -> attr.n().toInt()
         String::class -> attr.s()
         else -> throw UnknownTypeException(param.type.classifier)
+    }
+}
+
+internal fun parseNumber(str: String, clazz: KClass<*>): Number {
+    return when(clazz) {
+        Byte::class -> str.toByte()
+        Short::class -> str.toShort()
+        Int::class -> str.toInt()
+        Int::class -> str.toInt()
+        Long::class -> str.toLong()
+        Double::class -> str.toDouble()
+        Float::class -> str.toFloat()
+        else -> throw UnknownTypeException(clazz)
     }
 }
