628c81c8dae09927912aac954379b5ef1b43cf18
Jaitl <jaitl@outlook.com>
2021-09-22 13:45:44 +0300

    attribute update helper

--- integration-tests/src/test/kotlin/com/github/jaitl/dynamodb/EditObjectTest.kt
+++ integration-tests/src/test/kotlin/com/github/jaitl/dynamodb/EditObjectTest.kt
@@ -4,8 +4,8 @@ import com.github.jaitl.dynamodb.base.*
 import com.github.jaitl.dynamodb.mapper.Mapper
 import com.github.jaitl.dynamodb.mapper.mapAttribute
 import com.github.jaitl.dynamodb.mapper.numberAttribute
+import com.github.jaitl.dynamodb.mapper.updateAttribute
 import software.amazon.awssdk.services.dynamodb.model.AttributeAction
-import software.amazon.awssdk.services.dynamodb.model.AttributeValueUpdate
 import software.amazon.awssdk.services.dynamodb.model.UpdateItemRequest
 import java.time.Instant
 import kotlin.test.Test
@@ -30,10 +30,10 @@ internal class EditObjectTest : DynamoDbTestSuite() {
         val itemKey = mapper.writeObject(MyKey("1"))
 
         val updatedValues = mapOf(
-            "dataInt" to AttributeValueUpdate.builder()
-                .value(numberAttribute(4321))
-                .action(AttributeAction.PUT)
-                .build()
+            "dataInt" to updateAttribute(
+                attribute = numberAttribute(4321),
+                action = AttributeAction.PUT
+            )
         )
 
         val updateRequest = UpdateItemRequest.builder()
@@ -65,10 +65,10 @@ internal class EditObjectTest : DynamoDbTestSuite() {
         val newNested = NestedObject(4321, data.nested.dataInstant.plusSeconds(1000))
 
         val updatedValues = mapOf(
-            "nested" to AttributeValueUpdate.builder()
-                .value(mapAttribute(mapper.writeObject(newNested)))
-                .action(AttributeAction.PUT)
-                .build()
+            "nested" to updateAttribute(
+                attribute = mapAttribute(mapper.writeObject(newNested)),
+                action = AttributeAction.PUT
+            )
         )
 
         val updateRequest = UpdateItemRequest.builder()
@@ -97,10 +97,10 @@ internal class EditObjectTest : DynamoDbTestSuite() {
         val dataTwo = MyClassTwo("1", DtoTwo(4321L, Instant.now(), 4444.0))
 
         val updatedValues = mapOf(
-            "dto" to AttributeValueUpdate.builder()
-                .value(mapAttribute(mapper.writeObject(dataTwo.dto)))
-                .action(AttributeAction.PUT)
-                .build()
+            "dto" to updateAttribute(
+                attribute = mapAttribute(mapper.writeObject(dataTwo.dto)),
+                action = AttributeAction.PUT
+            )
         )
 
         val updateRequest = UpdateItemRequest.builder()
