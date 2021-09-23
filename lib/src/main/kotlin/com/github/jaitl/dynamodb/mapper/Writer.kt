a8b574da42773a5db04eadceaab186b1cd520209
Jaitl <jaitl@outlook.com>
2021-09-23 19:17:18 +0300

    fix dto info

--- lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Writer.kt
+++ lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/Writer.kt
@@ -14,10 +14,24 @@ class Writer(private val registry: ConverterRegistry = DEFAULT_REGISTRY) : KDyna
             throw NotDataClassTypeException("Type '${clazz}' isn't data class type")
         }
         val members = clazz.memberProperties
-        return members
+        val attrsMap = members
             .map { it to writeProperty(it, obj) }
             .filter { it.second != null }
             .associate { it.first.name to it.second!! }
+
+        return attrsMap + dtoClassInfo(clazz)
+    }
+
+    private fun dtoClassInfo(clazz: KClass<*>): Map<String, AttributeValue> {
+        val hasSealedParent = clazz.supertypes
+            .map { it.classifier as KClass<*> }
+            .any { it.isSealed }
+
+        if (hasSealedParent) {
+            return mapOf(DtoConverter.dtoFieldName to stringAttribute(clazz.qualifiedName!!))
+        }
+
+        return emptyMap()
     }
 
     private fun writeProperty(prop: KProperty1<out Any, *>, obj: Any): AttributeValue? {
--- lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/converter/DtoConverter.kt
+++ lib/src/main/kotlin/com/github/jaitl/dynamodb/mapper/converter/DtoConverter.kt
@@ -1,19 +1,22 @@
 package com.github.jaitl.dynamodb.mapper.converter
 
-import com.github.jaitl.dynamodb.mapper.*
+import com.github.jaitl.dynamodb.mapper.AttributeNotFoundException
+import com.github.jaitl.dynamodb.mapper.KDynamoMapperReader
+import com.github.jaitl.dynamodb.mapper.KDynamoMapperWriter
+import com.github.jaitl.dynamodb.mapper.mapAttribute
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
 
 object DtoConverter : TypeConverter<Any> {
-    private val dtoFieldName = "dto_class_name"
+    val dtoFieldName = "dto_class_name"
 
     override fun read(reader: KDynamoMapperReader, attr: AttributeValue, kType: KType): Any {
         val attrMap = attr.m()
         val realClazz = attrMap[dtoFieldName]?.s()
 
         if (realClazz == null) {
-            throw AttributeNotFoundException("Attribute '$dtoFieldName' for dto: '$kType' doesn't found")
+            throw AttributeNotFoundException("Attribute '$dtoFieldName' for DTO: '$kType' doesn't found")
         }
 
         val clazz = Class.forName(realClazz).kotlin
@@ -21,13 +24,8 @@ object DtoConverter : TypeConverter<Any> {
         return reader.readObject(attrMap, clazz)
     }
 
-    override fun write(writer: KDynamoMapperWriter, value: Any, kType: KType): AttributeValue {
-        val clazzName = value::class.qualifiedName!!
-
-        val dtoObj = writer.writeObject(value) + mapOf(dtoFieldName to stringAttribute(clazzName))
-
-        return mapAttribute(dtoObj)
-    }
+    override fun write(writer: KDynamoMapperWriter, value: Any, kType: KType): AttributeValue =
+        mapAttribute(writer.writeObject(value))
 
     override fun type(): KClass<Any> = throw NotImplementedError()
 }
